6630be5f3f43f8e87afcd8aba362dbc57b54c65c
SuperyajL <2859889037@qq.com>
2022-07-15 11:11:41 +0800

    不完美的空壳女各禾

--- src/views/City/index.vue
+++ src/views/City/index.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="city-container">
     <van-nav-bar title="城市列表" left-arrow @click-left="onClickLeft" />
-    <div class="box"></div>
+    <van-cell-group>
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+    </van-cell-group>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'city',
   created () { },
   data () {
     return {}
@@ -25,13 +29,13 @@ export default {
 
 <style scoped lang='less'>
 .city-container {
-.van-nav-bar {
-  position: fixed;
-  width: 100%;
-  top: 0;
+  .van-nav-bar {
+    position: fixed;
+    width: 100%;
+    top: 0;
   }
-}
-.box {
-  height: 1000px;
+  .van-cell-group {
+    margin-top: 50px;
+  }
 }
 </style>
--- src/views/Search/index.vue
+++ src/views/Search/index.vue
@@ -1,7 +1,55 @@
 <template>
-  <div>
-    <van-nav-bar left-arrow @click-left="onClickLeft"/>
-    <van-search v-model="value" disabled placeholder="请输入搜索关键词" />
+  <div class="search-container">
+    <van-nav-bar left-arrow @click-left="onClickLeft" />
+    <div class="search-box">
+      <van-button type="default" @click="goCs">
+        <span>杭州</span>
+        <van-icon name="arrow-down" />
+      </van-button>
+      <div class="last">
+        <van-icon name="search" />
+        <span>请输入小区或地址</span>
+      </div>
+    </div>
+    <van-icon name="upgrade" class="iclc" />
+    <van-sticky>
+      <van-dropdown-menu active-color="rgba(33,185,122)">
+        <van-dropdown-item title="区域" ref="item">
+          <div class="btn-box">
+            <van-button type="primary" @click="onConfirm">取消</van-button>
+            <van-button type="primary" @click="onConfirm">确定</van-button>
+          </div>
+        </van-dropdown-item>
+        <van-dropdown-item title="方式" ref="item1">
+          <div class="btn-box">
+            <van-button type="primary" @click="onConfirm1">取消</van-button>
+            <van-button type="primary" @click="onConfirm1">确定</van-button>
+          </div>
+        </van-dropdown-item>
+        <van-dropdown-item title="租金" ref="item2">
+          <div class="btn-box">
+            <van-button type="primary" @click="onConfirm2">取消</van-button>
+            <van-button type="primary" @click="onConfirm2">确定</van-button>
+          </div>
+        </van-dropdown-item>
+        <van-dropdown-item title="筛选" disabled/>
+
+        <van-cell @click="showPopup"
+          >筛选 <van-icon name="arrow-down" class="sx-icon"
+        /></van-cell>
+        <van-popup
+          v-model="show"
+          position="right"
+          :style="{ width: '80%', height: '100%' }"
+        >
+          <div class="btn-box">
+            <van-button type="primary">清除</van-button>
+            <van-button type="primary">确定</van-button>
+          </div>
+        </van-popup>
+      </van-dropdown-menu>
+    </van-sticky>
+    <div class="test"></div>
   </div>
 </template>
 
@@ -9,11 +57,31 @@
 export default {
   created () { },
   data () {
-    return {}
+    return {
+      show: false
+    }
   },
   methods: {
+    goCs () {
+      this.$router.push('/city')
+    },
+    showPopup () {
+      this.show = true
+    },
     onClickLeft () {
       this.$router.push('/home')
+    },
+    onConfirm () {
+      this.$refs.item.toggle()
+    },
+    onConfirm1 () {
+      this.$refs.item1.toggle()
+    },
+    onConfirm2 () {
+      this.$refs.item2.toggle()
+    },
+    onConfirm3 () {
+      this.$refs.item3.toggle()
     }
   },
   computed: {},
@@ -24,16 +92,99 @@ export default {
 </script>
 
 <style scoped lang='less'>
-div{
+.search-container {
+  * {
+    box-sizing: border-box;
+  }
+  .search-box {
+    position: absolute;
+    top: 7px;
+    width: 260px;
+    height: 34px;
+    z-index: 1;
+    margin: 0 10px 0 50px;
+    padding: 5px 5px 5px 8px;
+    border-radius: 3px;
+    background-color: #fff;
+    .van-button {
+      position: absolute;
+      top: 0;
+      left: 0;
+      height: 34px;
+      padding: 8px;
+      font-size: 14px;
+      .van-icon {
+        margin-left: 2px;
+        font-size: 12px;
+        color: #7f7f80;
+      }
+    }
+    .last {
+      margin-left: 59px;
+      padding-left: 4px;
+      font-size: 13px;
+      color: #9c9fa1;
+      line-height: 25px;
+      .van-icon {
+        margin-right: 4px;
+        font-size: 15px;
+      }
+    }
+  }
+  .iclc {
+    z-index: 1;
+    position: absolute;
+    top: 12px;
+    right: 25px;
+    font-size: 25px;
+    color: #fff;
+  }
+  .test {
+    height: 5000px;
+  }
+  .btn-box {
+    display: flex;
+    .van-button {
+      flex: 1;
+      height: 50px;
+      font-size: 18px;
+      color: #21b97a;
+      border: #fff;
+      background-color: #fff;
+      &:nth-child(2) {
+        flex: 2;
+        color: #fff;
+        background-color: #21b97a;
+      }
+    }
+  }
+}
+.van-dropdown-menu {
   position: relative;
-.van-search{
-  z-index: 1;
-  position: absolute;
-  top: 0;
-  width: 275px;
-  height: 34px;
-  background-color: #fff;
+  .van-cell {
+    width: 93.8px;
+    height: 48px;
+    line-height: 28px;
+    font-size: 15px;
+    padding-left: 30px;
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+  .van-popup {
+    .btn-box {
+      width: 100%;
+      position: fixed;
+      bottom: 0;
+    }
+  }
+  .sx-icon {
+    position: absolute;
+    right: 0;
+    top: 8px;
+    font-size: 8px;
+    color: #dcdee0;
+    z-index: 1;
+  }
 }
-}
-
 </style>
